cf357bc9c7caa08fa28876911ef7c57f98bfae80
Eric Sorenson <eric@puppet.com>
2020-07-10 10:57:47 -0700

    Update script, set -x

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+set -x
+
 cd ${GITHUB_WORKSPACE}
 FILENAME=$(git diff-tree -r --name-only --no-commit-id ${GITHUB_SHA} | grep yaml | head -1)
 
 if [[ -n ${FILENAME} ]]; then
   WORKFLOW=$(basename ${FILENAME} .yaml)
   echo "${INPUT_RELAY_PASSWORD}" | relay auth login ${INPUT_RELAY_USERNAME} -p
-  relay workflow replace ${WORKFLOW} -f ${FILENAME}
+  [[ $? == 0 ]] && relay workflow replace ${WORKFLOW} -f ${FILENAME}
+  exit $?
 else
   echo "Could not determine filename from git diff-tree at ${GITHUB_SHA}"
   exit 1
