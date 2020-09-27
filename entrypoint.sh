3c40d007dc66804cc70e07dd8c94b0a3ff6c2069
Rick Lane <rick.lane@puppet.com>
2020-09-27 16:40:39 -0700

    Default workflow name to base filename if not defined

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,18 @@ if [[ -n ${INPUT_RELAY_WORKFLOW_FILE} ]]; then
     echo 'ui_domain: ' ${INPUT_RELAY_HOST_UI} >> ~/.config/relay/config.yaml
   fi
 
+  WORKFLOW=${INPUT_RELAY_WORKFLOW}
+  if [[ -z ${WORKFLOW} ]]; then
+    FILENAME=$(basename -- "${INPUT_RELAY_WORKFLOW_FILE}")
+    WORKFLOW="${FILENAME%.*}"
+  fi
+
   echo "${INPUT_RELAY_PASSWORD}" | relay auth login ${INPUT_RELAY_USERNAME} -p
   #Skipping this check for now (until the latest Relay CLI has been released)
   #[[ $? == 0 ]] && relay workflow download ${INPUT_RELAY_WORKFLOW} >/dev/null
   # if [[ $? == 0 ]]; then
-  relay workflow replace ${INPUT_RELAY_WORKFLOW} -f ${INPUT_RELAY_WORKFLOW_FILE}
-  relay workflow run ${INPUT_RELAY_WORKFLOW} ${INPUT_RELAY_WORKFLOW_RUN_PARAMETERS}
+  relay workflow replace ${WORKFLOW} -f ${INPUT_RELAY_WORKFLOW_FILE}
+  relay workflow run ${WORKFLOW} ${INPUT_RELAY_WORKFLOW_RUN_PARAMETERS}
   exit $?
   # else
   #   echo "no workflow matching ${INPUT_RELAY_WORKFLOW} found, exiting cleanly"
