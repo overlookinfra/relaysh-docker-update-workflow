e4ef8bb0db9a3ad684fa5180be1f74d37a537845
Eric Sorenson <eric.sorenson@puppetlabs.com>
2020-10-05 23:49:53 -0700

    refactored to use functions

--- entrypoint.py
+++ entrypoint.py
@@ -5,68 +5,72 @@ import os
 from os import environ as env
 from subprocess import CalledProcessError, PIPE, STDOUT, run
 
+def configure_cli():
+  # translate from github env vars to the ones Relay CLI expects
+  if env['INPUT_RELAY_HOST_API']: 
+    os.putenv("RELAY_API_DOMAIN",env['INPUT_RELAY_HOST_API'])
+  if env['INPUT_RELAY_HOST_UI']:
+    os.putenv("RELAY_UI_DOMAIN",env['INPUT_RELAY_HOST_UI'])
+
+def setup_workflow(workflow_file, workflow_name):
+  # set up the file and workflow name to operate on
+  try:
+    stat = os.stat(workflow_file)
+    with open(workflow_file) as wf_obj:
+      workflow_contents = wf_obj.read
+  except:
+    print("Workflow file specified does not exist:", workflow_file)
+    exit(1)
+
+  if len(workflow_name) == 0:
+    workflow_name = os.path.basename(workflow_file).split('.')[0]
+
+  return workflow_file, workflow_name, workflow_contents
+
+def do_login():
+  try:
+    # BUG args should be a sequence but the relay command only sees the first one
+    login_command = "relay auth login " + env['INPUT_RELAY_USERNAME'] + " -p"
+    login_result = run(login_command, input=env['INPUT_RELAY_PASSWORD'], 
+      stderr=STDOUT, stdout=PIPE, check=True, shell=True, text=True)
+  except CalledProcessError as e:
+    print("Relay login failed: ", e, e.stdout)
+    exit(1)
+
+def download_and_replace(workflow_file,workflow_name,workflow_contents):
+  # download, compare, update if local version changed
+  try:
+    download_command = "relay workflow download " + workflow_name
+    download_result = run(download_command, capture_output=True, check=True, shell=True, text=True)
+  except CalledProcessError as e:
+    if e.stdout.find("could not find record"):
+      add_new_workflow(workflow_file,workflow_name)
+      pass
+
+  if workflow_contents != download_result.stdout:
+    try:
+      replace_command = "relay workflow replace " + workflow_name + " -f " + workflow_file
+      replace_result = run(replace_command, capture_output=True, check=True, shell=True, text=True)
+    except CalledProcessError as e:
+      print("Could not replace workflow", e, e.stdout)
+      exit(1)
+
+def add_new_workflow(workflow_file,workflow_name):
+  try:
+    add_command = "relay workflow add " + workflow_name + " -f" + workflow_file
+    add_result = run(add_command, capture_output=True, check=True, shell=True, text=True)
+  except CalledProcessError as e:
+    print("Could not add new workflow", e, e.stdout)
+    exit(1)
+  print("Added new workflow to your account, view it on the web:", add_result.stdout)
+  exit(0)
+
 os.chdir(env['GITHUB_WORKSPACE'])
 
-# translate from github env vars to the ones Relay CLI expects
-if env['INPUT_RELAY_HOST_API']: 
-  os.putenv("RELAY_API_DOMAIN",env['INPUT_RELAY_HOST_API'])
-if env['INPUT_RELAY_HOST_UI']:
-  os.putenv("RELAY_UI_DOMAIN",env['INPUT_RELAY_HOST_UI'])
+configure_cli()
 
-# set up the file and workflow name to operate on
-workflow_file = env['INPUT_RELAY_WORKFLOW_FILE']
-try:
-  stat = os.stat(workflow_file)
-  with open(workflow_file) as wf_obj:
-    local_workflow_contents = wf_obj.read
-except:
-  print("Workflow file specified does not exist:", workflow_file)
-  exit(1)
+do_login()
 
-workflow = env['INPUT_RELAY_WORKFLOW']
-if len(workflow) == 0:
-  workflow = os.path.basename(workflow_file).split('.')[0]
+workflow_file, workflow_name, workflow_contents = setup_workflow(env['INPUT_RELAY_WORKFLOW_FILE'], env['INPUT_RELAY_WORKFLOW'])
 
-# login sequence (TODO make this a function)
-try:
-  # this should be a sequence but the relay command only sees the first arg
-  login_command = "relay auth login " + env['INPUT_RELAY_USERNAME'] + " -p"
-  login_result = run(
-    login_command,
-    input=env['INPUT_RELAY_PASSWORD'],
-    stderr=STDOUT,
-    stdout=PIPE,
-    check=True,
-    shell=True,
-    text=True)
-except CalledProcessError as e:
-  print("Relay login failed: ", e, e.stdout)
-  exit(1)
-
-# download, compare, update if local version changed
-try:
-  download_command = "relay workflow download " + workflow
-  download_result = run(
-    download_command,
-    capture_output=True,
-    check=True,
-    shell=True,
-    text=True)
-except CalledProcessError as e:
-  if e.stdout.find("could not find record"):
-    # todo this should run a method to 'relay workflow add' it
-    pass
-
-if local_workflow_contents != download_result.stdout:
-  try:
-    replace_command = "relay workflow replace " + workflow + " -f " + workflow_file
-    replace_result = run(
-      replace_command,
-      capture_output=True,
-      check=True,
-      shell=True,
-      text=True
-    )
-  except CalledProcessError as e:
-    print("Could not replace workflow", e, e.stdout)
-    exit(1)
+download_and_replace(workflow_file,workflow_name,workflow_contents)
